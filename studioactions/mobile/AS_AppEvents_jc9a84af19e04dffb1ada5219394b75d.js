a5172c1fe5f54c8c5590a954337d47b4731cdb36
Bilisuma <bilitade2022@gmail.com>
2025-05-22 20:23:43 +0300

    bottom and app bar modified , for better look

---
+++ studioactions/mobile/AS_AppEvents_jc9a84af19e04dffb1ada5219394b75d.js
@@ -0,0 +1,8 @@
+function AS_AppEvents_jc9a84af19e04dffb1ada5219394b75d(eventobject) {
+    var self = this;
+    kony.application.setApplicationProperties({
+        "statusBarColor": "00adef00",
+        "statusBarHidden": false,
+        "navigationBarColor": "00adef00",
+    });
+}
---
+++ studioactions/mobile/appEvents.js
@@ -0,0 +1,13 @@
+define({
+    /*
+        This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_AppEvents_jc9a84af19e04dffb1ada5219394b75d: function AS_AppEvents_jc9a84af19e04dffb1ada5219394b75d(eventobject) {
+        var self = this;
+        kony.application.setApplicationProperties({
+            "statusBarColor": "00adef00",
+            "statusBarHidden": false,
+            "navigationBarColor": "00adef00",
+        });
+    }
+});
